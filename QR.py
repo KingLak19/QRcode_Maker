309a195d3744ac7607bc53f0885403ce8718ba3c
unknown <lakshananirudh19@gmail.com>
2021-05-10 21:23:47 +0530

    Added png

--- QR.py
+++ QR.py
@@ -1,18 +1,12 @@
-
 # Import QRCode from pyqrcode
 import pyqrcode
 import png
-from pyqrcode import QRCode
-
 
 # String which represents the QR code
-s = "https://www.youtube.com/watch?v=choF2JRgjHY"
+url = "https://www.youtube.com/watch?v=choF2JRgjHY"
 
 # Generate QR code
-url = pyqrcode.create(s)
-
-# Create and save the svg file naming "myqr.svg"
-url.svg("myqr.svg", scale = 8)
+qrcode = pyqrcode.create(url)
 
 # Create and save the png file naming "myqr.png"
-url.png('myqr.png', scale = 6)
+qrcode.png('QRCODE.png', scale = 6)
---
+++ README.md
@@ -0,0 +1,10 @@
+## How to install Modules I used :
+
+- pip install pyqrcode
+- pip install pypng
+
+## How it works :
+
+- Make a varible with the link that u want the QRcode to direct to
+- Use the .create function of pyqrcode to make the QR code
+- Use the .png function to save the QR code as a png
