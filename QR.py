3ec4276741149a4dc889161236255343f3b41c52
unknown <lakshananirudh19@gmail.com>
2021-05-11 12:45:10 +0530

    Different QR code

--- QR.py
+++ QR.py
@@ -3,10 +3,10 @@ import pyqrcode
 import png
 
 # String which represents the QR code
-url = "https://www.youtube.com/watch?v=choF2JRgjHY"
+url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
 # Generate QR code
 qrcode = pyqrcode.create(url)
 
 # Create and save the png file naming "myqr.png"
-qrcode.png('QRCODE.png', scale = 6)
+qrcode.png('MustScan.png', scale = 6)
